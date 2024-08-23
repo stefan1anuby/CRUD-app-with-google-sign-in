560319fda0b6d937d9dd6f3ab574d0acf77d29ad
stefan1anuby <stefaneduard2002@gmail.com>
2024-08-23 21:58:36 +0300

    deleted unused imports from main

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI, Depends, HTTPException
 from app.routers import users
 from sqlalchemy.orm import Session
 
-from app.database import get_db
-from app.crud import users as users_crud
 from app.database import Base, engine
 
 Base.metadata.create_all(bind=engine)
