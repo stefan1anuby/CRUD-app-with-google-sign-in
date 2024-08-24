481c5e3d902c84883366e02928edd0d4790a8887
stefan1anuby <stefaneduard2002@gmail.com>
2024-08-24 15:00:50 +0300

    deleted unnecesary code from tests

--- backend/app/tests/test_user_routes.py
+++ backend/app/tests/test_user_routes.py
@@ -2,33 +2,9 @@ import pytest
 
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
 
 from app.main import app  
 from app.database import Base, engine
-
-import os
-
-os.environ["IS_TEST_STAGE"] = "True"
-
-"""
-SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, connect_args={"check_same_thread": False}, 
-    poolclass=StaticPool)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Override the get_db dependency to use the test database
-def override_get_db():
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        db.close()
-
-app.dependency_overrides[get_db] = override_get_db
-"""
 
 # Initialize the test client
 client = TestClient(app)
